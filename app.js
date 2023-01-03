31af15d6a377ee77e57178221be7bdb0b8c343c5
superiorkid <bayiPetani@protonmail.com>
2023-01-03 18:15:57 +0800

    initial project complete

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 const PORT = 8001
 
+// cors
+app.use(cors())
+
 app.get('/', (req,res) => {
-    res.send("hello from backend")
+    res.status(200).json({
+        message: "Hello, this is basics auth MERN stack"
+    })
 })
 
 app.listen(PORT, () => {
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 const App = () => {
+    const [message, setMessage] = useState({})
+
+    useEffect(() => {
+        const msg = async () => {
+            await axios.get('http://localhost:8001').then((res) => {
+                setMessage(res.data)
+                console.log(message)
+                console.log(typeof message)
+            }).catch((err) => {
+                console.log(err)
+            })
+        }
+
+        msg()
+    })
+
     return (
         <div>
             <h2>Hello from frontend</h2>
+            {message.message}
         </div>
     )
 }
